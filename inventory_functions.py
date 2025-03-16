001132b312bad982e9ca0e9361687125395ae9fd
brookbrowning <brooksbrowning14@gmail.com>
2025-03-16 15:30:19 -0600

    changed flow of code, in the process of converting to csv vs json

--- inventory_functions.py
+++ inventory_functions.py
@@ -1,57 +1,162 @@
-my_inventory = [{'name': 'trident', 'type': 'weapon', 'damage': '3d6', 'range': 'melee', 'description' : None},
-               {'name': 'shocking grasp', 'type': 'spell', 'damage': '1d8', 'range': 'touch', 'description': None}, ]
+import csv
 
 
+# my_inventory = [{'name': 'trident', 'type': 'weapon', 'damage': '3d6', 'range': 'melee', 'description' : None},
+#                {'name': 'shocking grasp', 'type': 'spell', 'damage': '1d8', 'range': 'touch', 'description': None}, ]
+#
+# with open('inventory.csv', 'w', newline='') as csvfile:
+#     fieldnames = ['name', 'type', 'damage', 'range', 'description']
+#     writer = csv.DictWriter(csvfile, fieldnames= fieldnames)
+#     writer.writeheader()
+#     writer.writerows(my_inventory)
 
-def store():
-    """This function prompts user to select item type (weapon, spell, treasure, junk) and, if a valid option is chosen,
-     calls the name check function to see if the item already exists in the inventory"""
-    print("What would you like to store?")
-    inv_type = input(" Type 'w' for weapon\n Type 's' for spell\n Type 't' for treasure\n "
-                     "Or type 'u' for useless piece of crap: ").lower()
-    types = ['w', 's', 't', 'u']
-    if inv_type in types:
-        name_check(inv_type)
+
+def read_inv():
+    with open('inventory.json', 'r') as infile:
+        inventory = json.load(infile)
+    return inventory
+
+
+def is_inventory_empty():
+    file_size = len('inventory.json')
+    if file_size == 0:
+        return True
     else:
-        return
+        return False
 
-def overwrite(item_name):
-    print("overwriting!")
+def name_exists(name):
+    my_inventory = read_inv()
+    for val in my_inventory:
+        if val["name"] == name:
+            return True
+        else:
+            return False
 
 
-def name_check(type):
-    """This function takes an argument of item type (w, s, t, u) and prompts the user to input the item name.
-    This check the name against the current inventory, and if it already exists, asks the user if they would
-    like to overwrite the item information"""
-    name = input("What is the name of your object/spell?:")
+def overwrite(name):
+    print("overwriting")
 
-    #Check name in current inventory
+
+def view_item(name):
+    my_inventory = read_inv()
+    for index, entry in enumerate(my_inventory):
+        if entry['name'] == name:
+            for key in entry:
+                value = str(entry[key])
+                print(key.title(), ':', value.title())
+
+
+def view_inventory():
+    my_inventory = read_inv()
+    for index, entry in enumerate(my_inventory):
+        print(index+1, entry['name'].title())
+
+
+def name_check():
+    with open('inventory.json', 'r') as infile:
+        my_inventory = json.load(infile)
+    name = input("Please enter the name of your object/spell: ").lower()
     for val in my_inventory:
         if val["name"] == name:
             overwrite_choice = input("This name is already in use. Would you like to overwrite? (yes/no): ").lower()
             if overwrite_choice == 'yes':
-                overwrite(name)
+                print("overwriting!")
             elif overwrite_choice == 'no':
                 retry = input("Would you like to enter another name? (yes/no): ").lower()
                 if retry =='no':
                     return
                 else:
-                    name_check(type)
+                    name_check()
             else:
                 return
-    add_new_item(name, type)
+    add_new_item(name)
 
-def add_new_item(name, type):
-    """This function takes the item name and type as parameters and adds the item to the inventory based on attack
-    items (weapons/spells) or objects (treasure/junk)."""
-    if type == 'w' or type == 's':
-        damage = input("How much damage does this do?: ")
-        dam_range = input("What is the range?: ")
-        new_item = {'name' : name, 'type' : type, 'damage': damage, 'range' : dam_range, 'description': None}
-    else:
-        description = input("Enter a brief description: ")
-        new_item = {'name' : name, 'type' : type, 'damage': None, 'range' : None, 'description': description}
-    print(my_inventory)
-    my_inventory.append(new_item)
-    print(my_inventory)
+
+def object_type():
+    """This function prompts the user to determine a 'type' for their object, which determines which attributes it has"""
+    print("What kind item would you like to store?")
+    inv_type = input(" Type 'w' for weapon\n Type 's' for spell\n Type 't' for treasure\n "
+                     "Or type 'u' for useless piece of crap: ").lower()
+    types = ['w', 's', 't', 'u']
+    if inv_type in types:
+        return inv_type
+
+#
+# def add_new_item(name):
+#     """This function takes the item name and type as parameters and adds the item to the inventory based on attack
+#     items (weapons/spells) or objects (treasure/junk)."""
+#     ob_type = object_type()
+#     if ob_type == 'w' or ob_type == 's':
+#         damage = input("How much damage does this do?: ")
+#         dam_range = input("What is the range?: ")
+#         new_item = {'name' : name, 'type' : type, 'damage': damage, 'range' : dam_range, 'description': None}
+#     else:
+#         description = input("Enter a brief description: ")
+#         new_item = {'name' : name, 'type' : type, 'damage': None, 'range' : None, 'description': description}
+#
+#     with open('inventory.json', 'r') as f:
+#         current_inv = json.load(f)
+#     print(current_inv.update(new_item))
+#     # with open('inventory.json', 'w') as f:
+#     #     json.dump(current_inv, f)
+
+
+def choose_action():
+    correct_response = False
+    while correct_response is False:
+        print("What would you like to do?\n - Type 's' to store an item,\n - Type 'r' to retrieve an item,\n "
+                           "- Type 'i' to view a list of inventory,\n - Or type 'x' to exit\n")
+        user_input = input("I would like to: ").lower()
+        options = ['s', 'r', 'i', 'x']
+        if user_input in options:
+            correct_response = True
+            return user_input
+        else:
+            print("That is not an option, please try again\n")
+
+
+
+
+
+
+# def store():
+#     """This function prompts user to select item type (weapon, spell, treasure, junk) and, if a valid option is chosen,
+#      calls the name check function to see if the item already exists in the inventory"""
+#     print("What would you like to store?")
+#     inv_type = input(" Type 'w' for weapon\n Type 's' for spell\n Type 't' for treasure\n "
+#                      "Or type 'u' for useless piece of crap: ").lower()
+#     types = ['w', 's', 't', 'u']
+#     if inv_type in types:
+#         name_check(inv_type)
+#     else:
+#         return
+#
+#
+# def overwrite(item_name):
+#     print("overwriting!")
+#
+#
+# def name_check(type):
+#     """This function takes an argument of item type (w, s, t, u) and prompts the user to input the item name.
+#     This check the name against the current inventory, and if it already exists, asks the user if they would
+#     like to overwrite the item information"""
+#     name = input("What is the name of your object/spell?:")
+#
+#     #Check name in current inventory
+#     for val in my_inventory:
+#         if val["name"] == name:
+#             overwrite_choice = input("This name is already in use. Would you like to overwrite? (yes/no): ").lower()
+#             if overwrite_choice == 'yes':
+#                 overwrite(name)
+#             elif overwrite_choice == 'no':
+#                 retry = input("Would you like to enter another name? (yes/no): ").lower()
+#                 if retry =='no':
+#                     return
+#                 else:
+#                     name_check(type)
+#             else:
+#                 return
+#     add_new_item(name, type)
+#
+
 
--- ttrpg_inventory_management.py
+++ ttrpg_inventory_management.py
@@ -3,8 +3,9 @@
 # Description: This is an inventory management project (to be used when playing a ttrpg, like Dnd).
 # Goals: Create inventory objects that can be customized, stored, and recalled for quick reference during gameplay
 
-from inventory_functions import store
+from inventory_functions import *
 
+spacing = 3*'\n'
 
 # Introduction
 print("Welcome! To your own personal bag of holding! A near limitless space to store:\n")
@@ -14,11 +15,48 @@ print(" - Other knick-knacks you've accumulated")
 print("     (that you swear there's a use for)")
 print("\n")
 
-print("What would you like to do?\n - Type 's' to store an item,\n - Type 'r' to retrieve an item,\n "
-                   "- Type 'i' to view a list of inventory,\n - Or type '?' if you're generally confused\n")
-user_input = input("I would like to: ").lower()
+is_empty = is_inventory_empty()
+if is_empty is True:
+    print("Your inventory is currently empty. Would you like to store a new item?")
+    action = input("Type 'y' to store a new item. Type 'x' to exit: ").lower()
 
-if user_input == 's':
-    store()
+    if action == 'y':
+        new_name = input("Enter the name of your new item/spell: ").lower()
+        add_new_item(name=new_name)
+else:
+    exit_app = False
+    while exit_app is False:
+        function_choice = choose_action()
+        if function_choice == 's' or function_choice == 'r':
+            name = input("Please enter the name of your object/spell: ").lower()
+            already_exists = name_exists(name)
+            if already_exists is True:
+                if function_choice == 's':
+                    over_wr_choice = input("This name already exists. Would you like to overwrite? (y/n): ").lower()
+                    if over_wr_choice == "y":
+                        overwrite(name)
+                        print("Item info updated:")
+                        view_item(name)
+                        print(spacing)
+                else:
+                    print("This item's attributes are: ")
+                    view_item(name)
+                    update_choice = input("Would you like to update this item?")
+                    if update_choice == "y":
+                        overwrite(name)
+                        print("Item info updated:")
+                        view_item(name)
+            else:
+                if function_choice == "s":
+                    add_new_item(name)
+                else:
+                    print("\nThe item name entered does not exist. Please try again.", spacing)
+        elif function_choice == 'i':
+            print("Your current inventory: ")
+            view_inventory()
+            print(spacing)
+        else:
+            exit_app = True
+
 
 
